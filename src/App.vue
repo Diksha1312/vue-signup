e9fcad02c71c44f46a577b196306555865added6
Diksha1312 <gdiksha1312@gmail.com>
2025-07-24 16:19:49 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SignupForm />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import SignupForm from './components/SignupForm.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignupForm
   }
 }
 </script>
@@ -22,5 +24,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #f5f5f5;
 }
 </style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" v-model="email" required>
+  </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: ''
+        };
+    },
+}
+</script>
+
+<style>
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    padding: 40px;
+    background: white;
+    border-radius: 10px;
+    text-align: left;
+}
+label {
+    display: inline-block;
+    font-weight: bold;  
+    color: #aaa;
+    margin: 25px 0 15px;
+    font-size: 0.9em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+}
+
+input {
+    display: block;
+    width: 100%;
+    padding: 10px 6px;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    box-sizing: border-box;
+    color: #555;    
+}
+</style>
